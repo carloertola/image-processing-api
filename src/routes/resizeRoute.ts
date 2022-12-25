bd9ab2cf4e0aca21aeb5f56b4d34b285cbc6d82f
carloertola <carloertola@gmail.com>
2022-12-25 22:56:36 +0300

    added code to display images and resize into images with details

--- src/routes/resizeRoute.ts
+++ src/routes/resizeRoute.ts
@@ -1,23 +1,28 @@
 import express from 'express';
 import path from 'path';
-import { CLIENT_RENEG_WINDOW } from 'tls';
 import resizer from '../utilities/resizeImage';
 
 const resize = express.Router();
 
 // passing query parameters to function that handles resize if they exist
 resize.get('/', async (req, res) => {
-    if(req.query.filename) {
-        let filename = req.query.filename,
+    if(req.query.filename && req.query.width && req.query.height) {
+        let filename = (req.query.filename) as string,
         width = Number(req.query.width),
         height = Number(req.query.height);
-        const filePath = path.join(__dirname,'..','..','thumb_images','resized_', filename as string);
 
-        resizer(filename as string, width, height);
-        res.send('Image successfully resized');
+        resizer(filename, width, height);
+
+        const resImg = path.join(__dirname, '..', '..', 'thumb_images', 'resized_' + 
+        req.query.width + 'x' + req.query.height + '_' + filename);
+
+        res.sendFile(resImg);
+    } else if(req.query.filename) {
+        const fullImg = path.join(__dirname, '..', '..', 'full_size_images', req.query.filename as string);
+        res.sendFile(fullImg);
     } else {
-        res.send('Please enter a query with your details like this example:</br>' +
-        '<code>http://localhost:3000/api/resize?filename=encenadaport.jpg&width=200&height=400</code>');
+        res.send('Please enter a query with filename, width, and height like this:</br>' +
+        '<code>http://localhost:3000/api/resize?filename=encenadaport.jpg&width=200&height=200</code>');
     }
 });
 
