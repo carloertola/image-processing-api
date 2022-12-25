1905174f9ed4c51a50f61374dd7947ba8c0375ce
carloertola <carloertola@gmail.com>
2022-12-25 22:57:50 +0300

    refined the way the resized image is saved

--- src/utilities/resizeImage.ts
+++ src/utilities/resizeImage.ts
@@ -1,14 +1,12 @@
 import sharp from 'sharp';
 import path from 'path';
-import resize from '../routes/resize';
+import resize from '../routes/resizeRoute';
 
 const resizer = async (fn: string, w: number, h: number) => {
-    console.log('Entered resizer function');
-    console.log(path.join(__dirname + "..", "..", "full_size_images", fn));
     try {
         await sharp(path.join(__dirname, "..", "..", "full_size_images", fn))
         .resize(w, h)
-        .toFile(path.join(__dirname, "..", "..", "thumb_images", `resized_${fn}`))
+        .toFile(path.join(__dirname, "..", "..", "thumb_images", `resized_${w}x${h}_${fn}`))
     } catch (err) {
         console.log(`Oops! There seems to be an error:</br>${err}`);
     }
