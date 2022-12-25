2c80928495b9821c6e47f6e873513b670b4b42eb
carloertola <carloertola@gmail.com>
2022-12-25 13:14:43 +0300

    Updated interface

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Main Router Page');
+    res.send('Go to the app: <a href="/api">Image API</a>');
 });
 
 app.use('/api', routes);
