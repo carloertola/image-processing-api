f43ceac97d26da6ef06c94f662b6ca71a69ba6a9
carloertola <carloertola@gmail.com>
2022-12-28 02:28:49 +0300

    fixed up configuration

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,16 @@ import routes from './routes/index';
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Go to the app: <a href="/api">Image API</a>');
-});
-
 app.use('/api', routes);
+app.set('view engine', 'pug');
+
+// redirecting localhost:3000 to localhost:3000/api
+app.get('/', (req, res) => {
+  res.redirect('/api');
+});
 
 app.listen(port, () => {
-    console.log(`Server up and running on localhost:${port}`);
+  console.log(`Server up and running on localhost:${port}`);
 });
+
+export default app;
