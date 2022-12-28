5569e0a86712ef26dafb64c80589b209a2808475
carloertola <carloertola@gmail.com>
2022-12-28 14:29:23 +0300

    added some tests

---
+++ src/tests/utilities/listFilesSpec.ts
@@ -0,0 +1,10 @@
+import lsFiles from '../../utilities/listFiles';
+
+describe('tests the listFiles function', async () => {
+  it('should display filenames of existing files as expected', () => {
+    expect(lsFiles()).toEqual(
+      'encenadaport.jpg | fjord.jpg | ' +
+      'icelandwaterfall.jpg | palmtunnel.jpg | santamonica.jpg'
+    );
+  });
+});
---
+++ src/tests/utilities/resizeSpec.ts
@@ -0,0 +1,21 @@
+import fs from 'fs';
+import path from 'path';
+import resize from '../../utilities/resize';
+
+describe('tests the resize function', () => {
+  it('should crete a resized file in thumb_images folder', () => {
+    resize('fjord.jpg', 200, 200);
+    const fileExistsCheck = fs.existsSync(
+      path.join(
+        __dirname,
+        '..',
+        '..',
+        '..',
+        'images',
+        'thumb_images',
+        '200x200 fjord.jpg'
+      )
+    );
+    expect(fileExistsCheck).toBe(true);
+  });
+});
