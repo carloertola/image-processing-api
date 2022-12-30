8d8a5200316cc5b0f1439db31033d229efa00d7a
carloertola <carloertola@gmail.com>
2022-12-30 23:34:35 +0300

    fixed up directory issue

--- src/tests/utilities/listFilesSpec.ts
+++ src/tests/utilities/listFilesSpec.ts
@@ -4,7 +4,7 @@ describe('tests the listFiles function', async () => {
   it('should display filenames of existing files as expected', () => {
     expect(lsFiles()).toEqual(
       'encenadaport.jpg | fjord.jpg | ' +
-      'icelandwaterfall.jpg | palmtunnel.jpg | santamonica.jpg'
+        'icelandwaterfall.jpg | palmtunnel.jpg | santamonica.jpg'
     );
   });
 });
