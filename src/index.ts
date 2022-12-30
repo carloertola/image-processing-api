ad1fcd05c59cb8e68298ea0d43d1dde1d6db194c
carloertola <carloertola@gmail.com>
2022-12-30 23:32:37 +0300

    added some type definitions

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
 import express from 'express';
 import routes from './routes/index';
 
-const app = express();
+const app: express.Application = express();
 const port = 3000;
 
 app.use('/api', routes);
 app.set('view engine', 'pug');
 
 // redirecting localhost:3000 to localhost:3000/api
-app.get('/', (req, res) => {
+app.get('/', (req: express.Request, res: express.Response) => {
   res.redirect('/api');
 });
 
-app.listen(port, () => {
+app.listen(port, (): void => {
   console.log(`Server up and running on localhost:${port}`);
 });
 
